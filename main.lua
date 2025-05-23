6c03a0aff516cc40061db80daeeda4d1e094b3f7
Somethingcom515 <55041595+Somethingcom515@users.noreply.github.com>
2025-05-24 08:30:21 +1200

    ‎

--- main.lua
+++ main.lua
@@ -2714,7 +2714,6 @@ end
 function SEALS.recursive_extra(table_return_table, index)
     if #table_return_table == 0 then return elseif #table_return_table == 1 then return table_return_table[1] end
     if not index then index = 1 end
-	
     local ret = table_return_table[index]
     if index <= #table_return_table then
         local function getDeepest(tbl)
